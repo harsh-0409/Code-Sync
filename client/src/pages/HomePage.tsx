afa2dc77cdf0545df5c5ccd03e37cae9843fbf97
Harsh <rajh87897@gmail.com>
2024-12-07 16:15:57 +0530

    Added my name to the landing page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
 import illustration from "@/assets/illustration.svg"
 import FormComponent from "@/components/forms/FormComponent"
 import Chatbot from "@/components/Chatbot";
-// import Footer from "@/components/common/Footer";
 
 function HomePage() {
     return (
+        
         <div className="flex min-h-screen flex-col items-center justify-center gap-16">
             <div className="my-12 flex h-full min-w-full flex-col items-center justify-evenly sm:flex-row sm:pt-0">
                 <div className="flex w-full animate-up-down justify-center sm:w-1/2 sm:pl-4">
@@ -14,14 +14,18 @@ function HomePage() {
                         className="mx-auto w-[250px] sm:w-[400px]"
                     />
                 </div>
+                <div className="text-center">
+                <h2 className="text-xl font-bold">Developed by Harsh</h2>
+                <h3 className="text-xl font-bold">Section 32, Team 17</h3>
+            </div>
                 <div className="flex w-full items-center justify-center sm:w-1/2">
                     <FormComponent />
                 </div>
             </div>
             <Chatbot />
-            {/* <Footer /> */}
+            
         </div>
     )
 }
 
-export default HomePage
+export default HomePage;
